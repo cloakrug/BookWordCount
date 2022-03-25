6a61a45f81397759b36e16f98f91f59c7f9af532
prestonwin78 <prestonwin78@gmail.com>
2022-03-24 21:21:15 -0500

    Add BookContext

---
+++ Models/BookContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookWordCount.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Book> Books { get; set; } = null!;
+        public DbSet<BookGenre> BookGenre { get; set; } = null!;
+        public DbSet<Genre> Genres { get; set; } = null!;
+        public DbSet<MajorGenre> MajorGenres { get; set; } = null!;
+        public DbSet<PageCount> PageCounts { get; set; } = null!;
+        public DbSet<WordCount> WordCounts { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
