21b51331d492dbefd2a1f621c23e55d66d55ac25
prestonwin78 <prestonwin78@gmail.com>
2022-03-24 21:53:59 -0500

    Add crud operations to Books

--- Services/BookService.cs
+++ Services/BookService.cs
@@ -4,5 +4,40 @@ namespace BookWordCount.Services
 {
     public class BookService
     {
+        private readonly BookContext _ctx;
+
+        public BookService(BookContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return _ctx.Books;
+        }
+
+        //public IEnumerable<Book> GetBooksByGenre()
+        //{
+        //}
+
+        public Book AddBook(Book book)
+        {
+            _ctx.Books.Add(book);
+            _ctx.SaveChanges();
+            return book;
+        }
+
+        public Book UpdateBook(Book book)
+        {
+            _ctx.Update(book);
+            _ctx.SaveChanges();
+            return book;
+        }
+
+        public void RemoveBook(Book book)
+        {
+            _ctx.Books.Remove(book);
+            _ctx.SaveChanges();
+        }
     }
 }
