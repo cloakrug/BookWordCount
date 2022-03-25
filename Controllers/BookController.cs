3b14c5b9351fa07f0e8bbb2ad2708093c68c15f3
prestonwin78 <prestonwin78@gmail.com>
2022-03-24 21:58:23 -0500

    Add GetBooks endpoint

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,6 +1,24 @@
-﻿namespace BookWordCount.Controllers
+﻿using BookWordCount.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWordCount.Controllers
 {
-    public class BookController
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
     {
+        private readonly BookService _bookService;
+
+        public BookController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("GetBooks", Name = nameof(GetBooks))]
+        public IActionResult GetBooks()
+        {
+            // TODO: set up mapper
+            return Ok(_bookService.GetBooks());
+        }
     }
 }
